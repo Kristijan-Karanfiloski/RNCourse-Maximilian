54a63ded31040cc4f9195e1d37e999bd2fae0a69
kikaccc29 <kristijan.karanfiloski@gmail.com>
2023-11-29 15:29:58 +0100

    react native maximilian

--- App.js
+++ App.js
@@ -1,28 +1,95 @@
-import { StyleSheet, Text, View, Button } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Button,
+  ScrollView,
+  FlatList,
+} from "react-native";
+import { useState } from "react";
 
 export default function App() {
+  const [enteredGoalText, setEnteredGoalText] = useState("");
+  const [courseGoals, setCourseGoals] = useState([]);
+  const goalInputHandler = (enteredText) => {
+    setEnteredGoalText(enteredText);
+    // console.log("ENTERED TEXT", enteredText);
+  };
+  const addGoalHandler = () => {
+    //not the bese way to update state if the prevuious state depends on the current state
+    // setCourseGoals([...courseGoals, enteredGoalText]);
+    setCourseGoals((currentCourseGoals) => [
+      ...currentCourseGoals,
+      { text: enteredGoalText, id: Math.random().toString() },
+    ]);
+    console.log(enteredGoalText);
+  };
+
   return (
-    <View style={styles.container}>
-      <View>
-        <Text style={styles.dummyText}>Another piece of text !!!</Text>
+    <View style={styles.appContainer}>
+      <View style={styles.inputContainer}>
+        <TextInput
+          onChangeText={goalInputHandler}
+          style={styles.textInput}
+          placeholder="Your course goal!"
+        />
+        <Button onPress={addGoalHandler} title="Add Goal" />
       </View>
-      <Text style={styles.dummyText}>Hello World!!!!</Text>
-      <Button title="Tap me" />
+      <View style={styles.goalsContainer}>
+        {courseGoals.length > 0 ? (
+          <FlatList
+            data={courseGoals}
+            keyExtractor={(item) => item.id}
+            renderItem={(itemData) => {
+              console.log(itemData.item.text);
+              return (
+                <View style={styles.goalItem}>
+                  <Text style={styles.goalText}>{itemData.item.text}</Text>
+                </View>
+              );
+            }}
+          />
+        ) : (
+          <Text>Enter goals...</Text>
+        )}
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  appContainer: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    paddingTop: 50,
+    paddingHorizontal: 16,
   },
-  dummyText: {
-    margin: 16,
-    borderWidth: 2,
-    borderColor: "blue",
-    padding: 16,
+  inputContainer: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginBottom: 24,
+    borderBottomWidth: 1,
+    borderBottomColor: "#cccccc",
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: "#cccccc",
+    width: "70%",
+    marginRight: 8,
+    padding: 8,
+  },
+  goalsContainer: {
+    flex: 5,
+  },
+  goalItem: {
+    margin: 8,
+    borderRadius: 6,
+    padding: 8,
+    backgroundColor: "#5e0acc",
+  },
+  goalText: {
+    color: "white",
   },
 });
